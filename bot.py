e315a896f7d995c0698db78d204747b6a66f9601
Rizki636 <76016674+Rizki636@users.noreply.github.com>
2021-08-01 09:33:20 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,10 +4,7 @@ from telebot import types
 
 bot = telebot.TeleBot(token=os.getenv('TG_BOT_TOKEN'))
 
-@rizki.message_handler(commands=["start"])
-def text(message):
-    chatid = message.chat.id
-    rizki.send_photo(chatid, open('1566206027.png','rb'))
+@bot.message_handler(commands=["start"])
 def awal_bot(message):
     tahap1 = types.InlineKeyboardMarkup()
     tahap2 = types.InlineKeyboardButton(text="CHANNEL STORE",url="https://t.me/AZ_Dan_Rizki_Store636")
@@ -17,14 +14,14 @@ def awal_bot(message):
     tahap1.add(tahap2)
     tahap1.add(tahap3,tahap4)
     tahap1.add(tahap5)
-    rizki.send_message(message.chat.id,"List Harga Yang Kami Jual\n===========\nYouTube Premium 4 Bulan = 3k\nYouTube Premium 1 Bulan = 1k\nNokos Telegram USA = 1.200\nNokos Telegram ID = 1.900\nNokos WhatsApp = 1k\n===========\nMinat Hubungi @Nokos_Rizki_Bot\n==========\nCommand yang tersedia di bot ini =\n/admin untuk melihat kontak admin",reply_markup=tahap1)
-    rizki.send_message(message.chat.id,"Bot By @Rizki636 --> Github --> Heroku --> Telegram")
+    bot.send_message(message.chat.id,"List Harga Yang Kami Jual\n===========\nYouTube Premium 4 Bulan = 3k\nYouTube Premium 1 Bulan = 1k\nNokos Telegram USA = 1.200\nNokos Telegram ID = 1.900\nNokos WhatsApp = 1k\n===========\nMinat Hubungi @Nokos_Rizki_Bot\n==========\nCommand yang tersedia di bot ini =\n/admin untuk melihat kontak admin",reply_markup=tahap1)
+    bot.send_message(message.chat.id,"Bot By @Rizki636 --> Github --> Heroku --> Telegram")
 
-@rizki.message_handler(commands=["admin"])
+@bot.message_handler(commands=["admin"])
 def awal_bot(message):
     dest1 = types.InlineKeyboardMarkup()
     dest2 = types.InlineKeyboardButton(text="KONTAK ADMIN",url="https://t.me/Rizki636")
     dest1.add(dest2)
-    rizki.send_message(message.chat.id,"ini adalah kontak admin",reply_markup=dest1)
+    bot.send_message(message.chat.id,"ini adalah kontak admin",reply_markup=dest1)
 print("bot berjalan")
-rizki.polling()
+bot.polling()
