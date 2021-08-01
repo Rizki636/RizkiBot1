54e65a24f4a1a0b3ef3e4154e1f7944f428ff63c
Rizki636 <76016674+Rizki636@users.noreply.github.com>
2021-08-01 10:35:52 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,5 +25,18 @@ def awal_bot(message):
     dest2 = types.InlineKeyboardButton(text="KONTAK ADMIN",url="https://t.me/Rizki636")
     dest1.add(dest2)
     bot.send_message(message.chat.id,"ini adalah kontak admin",reply_markup=dest1)
+    keybord1 = types.ReplyKeyboardMarkup()
+    keybord2 = types.KeyboardButton(text="ga ngapa ngapain")
+    keybord3 = types.KeyboardButton(text="mau tanya sesuatu")
+    keybord1.add(keybord2,keybord3)
+    bot.send_message(message.chat.id,"mau ngapain?, ko cari cari no admin?",reply_markup=keybord1)
+    
+@bot.message_handler(regexp='ga ngapa ngapain')
+def text(message):
+    bot.reply_to(message, """owh""")
+    
+@bot.message_handler(regexp='mau tanya sesuatu')
+def text(message):
+    bot.reply_to(message, """owh, ya udah lanjut tanya sana""")
 print("bot berjalan")
 bot.polling()
